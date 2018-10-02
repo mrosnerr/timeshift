1c91b939e5503adc03fab7e303bb40ec522a60ae
Meir Rosner <mrosner10@gmail.com>
2018-10-02 17:48:05 -0400

    Refactored out the web view into a seperate file

--- src/App.js
+++ src/App.js
@@ -1,75 +1,11 @@
 import React, { Component } from 'react';
-import { DateTime } from 'luxon';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import MetaTags from 'react-meta-tags';
+import Hidden from '@material-ui/core/Hidden';
+import ViewWeb from './ViewWeb';
 import './App.css';
-import DateTimezonePair from './DateTimezonePair';
-import { PlaylistAdd } from '@material-ui/icons';
 
 class App extends Component {
-  constructor() {
-    super();
-    const cached = localStorage.getItem('records');
-    const records = cached ? JSON.parse(cached) : [];
-    console.log('records', records);
-    this.state = {
-      records,
-      utc: DateTime.utc()
-    };
-  }
-
-  componentDidMount() {
-    if (this.state.records.length === 0) {
-      this.addRecord();
-      this.addRecord();
-    }
-  }
-
-  addRecord = () => {
-    const record = {}
-
-    const records = this.state.records;
-
-    records.push(record);
-
-    this.setState({ records })
-    localStorage.setItem('records', JSON.stringify(records));
-  }
-
-  updateHandler = (utc) => {
-    this.setState({ utc });
-  }
-
-  removeHandler = (id) => {
-    const records = this.state.records;
-
-    records.splice(id, 1);
-
-    this.setState({ records });
-    localStorage.setItem('records', JSON.stringify(records));
-  }
-
   render() {
-
-    const rows = this.state.records.map((record, index) => {
-      const readOnly = (index !== 0);
-
-      return (
-        <DateTimezonePair
-          key={index}
-          id={index}
-          readOnly={readOnly}
-          utc={this.state.utc}
-          updateHandler={this.updateHandler}
-          removeHandler={this.removeHandler}
-        />
-      )
-    });
-
     return (
       <div className="App">
         <MetaTags>
@@ -82,19 +18,9 @@ class App extends Component {
         <p className="App-intro">
           A utility for deriving dates + times across timezones
         </p>
-          <Table style={{ width: 700, margin: 'auto' }}>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{textAlign: 'center', fontSize: 20 }}>Timezone</TableCell>
-              <TableCell style={{textAlign: 'center', fontSize: 20 }}>Date + Time</TableCell>
-              <TableCell style={{textAlign: 'center', fontSize: 20 }}>Remove</TableCell>
-            </TableRow>
-          </TableHead>
-            <TableBody>
-              {rows}
-            </TableBody>
-          </Table>
-        <PlaylistAdd className='iconHover' style={{ fontSize: 30, height: 75 }} onClick={this.addRecord}/>
+        <Hidden xsDown>
+          <ViewWeb />
+        </Hidden>
       </div>
     );
   }
